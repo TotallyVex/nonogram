1c41d30ce3a6bd4e103fe633ca17dc36a31d8cb9
Jon Müller <71830212+TotallyVex@users.noreply.github.com>
2025-06-24 20:13:53 +0200

    added central state management & reset button

--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -1,17 +1,51 @@
 import { CellState } from "@/types";
+import { cn } from "@sglara/cn";
 
 export default function Cell({
   correctState,
   rowIdx,
   colIdx,
+  updateCellState,
+  currentState,
 }: {
   correctState: CellState;
   rowIdx: number;
   colIdx: number;
+  updateCellState: (row: number, col: number, state: CellState) => void;
+  currentState: CellState;
 }) {
+  const handleLeftClick = () => {
+    if (currentState !== CellState.EMPTY) return;
+
+    if (correctState === CellState.FILLED) {
+      updateCellState(rowIdx, colIdx, CellState.FILLED);
+      return;
+    }
+
+    console.log("Wrong cell clicked");
+  };
+
+  const handleRightClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (currentState !== CellState.EMPTY) return;
+
+    if (correctState === CellState.BLOCKED) {
+      updateCellState(rowIdx, colIdx, CellState.BLOCKED);
+      return;
+    }
+    console.log("Wrong cell clicked");
+  };
+
   return (
-    <div className="border border-gray-300 flex items-center justify-center w-[50px] h-[50px]">
-      {correctState}
+    <div
+      className={cn(
+        "border border-gray-300 flex items-center justify-center w-[50px] h-[50px] text-black text-5xl select-none cursor-pointer",
+        currentState === CellState.FILLED ? "bg-stone-800" : "bg-white"
+      )}
+      onClick={handleLeftClick}
+      onContextMenu={handleRightClick}
+    >
+      {currentState === CellState.BLOCKED ? "X" : ""}
     </div>
   );
 }
--- src/components/playingField.tsx
+++ src/components/playingField.tsx
@@ -19,41 +19,62 @@ export default function PlayingField({ puzzle }: { puzzle: string[] }) {
     });
   };
 
+  const handleReset = () => {
+    console.log("Resetting puzzle state");
+    setPuzzleState(
+      Array.from({ length: puzzle.length }, () =>
+        Array.from({ length: puzzle[0].length }, () => CellState.EMPTY)
+      )
+    );
+  };
+
   return (
-    <div
-      className="grid"
-      style={{
-        gridTemplateColumns: `repeat(${puzzle[0].length + 2}, 1fr)`,
-        gridTemplateRows: `repeat(${puzzle.length + 2}, 1fr)`,
-      }}
-    >
-      {/* keep bottom left corner free */}
-      <div className="w-auto h-auto col-span-2 row-span-2" />
+    <div className="flex flex-col items-center justify-center w-full h-full gap-8">
+      <button
+        onClick={handleReset}
+        className="bg-indigo-400 px-4 py-2 rounded-md cursor-pointer hover:bg-indigo-500 transition-colors duration-300"
+      >
+        <span className="text-lg font-semibold mb-4">Reset Puzzle</span>
+      </button>
+      <div
+        className="grid"
+        style={{
+          gridTemplateColumns: `repeat(${puzzle[0].length + 2}, 1fr)`,
+          gridTemplateRows: `repeat(${puzzle.length + 2}, 1fr)`,
+        }}
+      >
+        {/* keep bottom left corner free */}
+        <div className="w-auto h-auto col-span-2 row-span-2" />
 
-      {puzzle[0].split("").map((header, idx) => (
-        <div
-          key={`header-${idx}`}
-          className="w-auto h-auto border border-gray-300 flex items-center justify-center row-span-2 col-span-1"
-        >
-          test
-        </div>
-      ))}
-
-      {puzzle.map((row, rowIdx) => (
-        <Fragment key={`row-${rowIdx}`}>
-          <div className="flex items-center justify-center h-auto col-span-2 border border-gray-300">
+        {puzzle[0].split("").map((header, idx) => (
+          <div
+            key={`header-${idx}`}
+            className="w-auto h-auto border border-gray-300 flex items-center justify-center row-span-2 col-span-1"
+          >
             test
           </div>
-          {row.split("").map((cell, colIdx) => (
-            <Cell
-              rowIdx={rowIdx}
-              colIdx={colIdx}
-              correctState={cell === "X" ? CellState.BLOCKED : CellState.FILLED}
-              key={`cell-${rowIdx}-${colIdx}`}
-            />
-          ))}
-        </Fragment>
-      ))}
+        ))}
+
+        {puzzle.map((row, rowIdx) => (
+          <Fragment key={`row-${rowIdx}`}>
+            <div className="flex items-center justify-center h-auto col-span-2 border border-gray-300">
+              test
+            </div>
+            {row.split("").map((cell, colIdx) => (
+              <Cell
+                rowIdx={rowIdx}
+                colIdx={colIdx}
+                correctState={
+                  cell === "X" ? CellState.BLOCKED : CellState.FILLED
+                }
+                updateCellState={updateCellState}
+                currentState={puzzleState[rowIdx][colIdx]}
+                key={`cell-${rowIdx}-${colIdx}`}
+              />
+            ))}
+          </Fragment>
+        ))}
+      </div>
     </div>
   );
 }
